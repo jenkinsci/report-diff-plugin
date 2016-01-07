8f24c1551ed58bc303924c27d3da5739a27da02e
Stanislav Baiduzhyi <baiduzhyi.devel@gmail.com>
2016-01-07 14:06:26 +0100

    Chart seems to be working.

--- src/main/java/hudson/plugins/report/rpms/RpmsReportProjectAction.java
+++ src/main/java/hudson/plugins/report/rpms/RpmsReportProjectAction.java
@@ -28,9 +28,11 @@ import hudson.model.Job;
 import hudson.model.Run;
 import java.io.File;
 import java.nio.file.Files;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import static hudson.plugins.report.rpms.Constants.RPMS_ALL;
 import static hudson.plugins.report.rpms.Constants.RPMS_NEW;
 import static hudson.plugins.report.rpms.Constants.RPMS_REMOVED;
 
@@ -57,26 +59,46 @@ public class RpmsReportProjectAction implements Action {
         return "rpms";
     }
 
-    public List<String> getNewRpms() {
-        return readFile(RPMS_NEW);
-    }
+    public RpmsReportChart getChartData() {
+        List<String> builds = new ArrayList<>();
+        List<String> installed = new ArrayList<>();
+        List<String> removed = new ArrayList<>();
+        List<String> total = new ArrayList<>();
+        for (Run run : job.getBuilds()) {
 
-    public List<String> getRemovedRpms() {
-        return readFile(RPMS_REMOVED);
-    }
-
-    private List<String> readFile(String fileName) {
-        try {
-            Run build = job.getLastBuild();
-            if (build != null) {
-                File file = new File(build.getRootDir(), fileName);
-                if (file.exists() && file.isFile() && file.canRead()) {
-                    return Files.readAllLines(file.toPath());
-                }
+            File allFile = new File(run.getRootDir(), RPMS_ALL);
+            if (!checkFile(allFile)) {
+                continue;
             }
-        } catch (Exception ignore) {
+            builds.add(run.getDisplayName());
+            total.add(fileLines(allFile));
+            installed.add(fileLines(new File(run.getRootDir(), RPMS_NEW)));
+            removed.add(fileLines(new File(run.getRootDir(), RPMS_REMOVED)));
+
+            if (builds.size() == 10) {
+                break;
+            }
         }
-        return Collections.emptyList();
+        Collections.reverse(builds);
+        Collections.reverse(installed);
+        Collections.reverse(removed);
+        Collections.reverse(total);
+        return new RpmsReportChart(builds, installed, removed, total);
+    }
+
+    private boolean checkFile(File f) {
+        return f.exists() && f.isFile() && f.canRead();
+    }
+
+    private String fileLines(File f) {
+        if (checkFile(f)) {
+            try {
+                int number = Files.readAllLines(f.toPath()).size();
+                return String.valueOf(number);
+            } catch (Exception ignore) {
+            }
+        }
+        return "0";
     }
 
 }
