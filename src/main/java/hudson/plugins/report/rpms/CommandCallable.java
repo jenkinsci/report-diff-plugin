7e319056a4e03699aee498f642480929f2667a05
Jiri <jvanek@redhat.com>
2023-02-21 13:50:50 +0100

    Next to command, it now can read also file
    thus being much more versatile

--- src/main/java/hudson/plugins/report/rpms/CommandCallable.java
+++ src/main/java/hudson/plugins/report/rpms/CommandCallable.java
@@ -25,13 +25,17 @@ package hudson.plugins.report.rpms;
 
 import hudson.FilePath.FileCallable;
 import hudson.remoting.VirtualChannel;
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
+
 import org.jenkinsci.remoting.RoleChecker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -49,34 +53,41 @@ public class CommandCallable implements FileCallable<List<String>> {
     }
 
     @Override
-    public List<String> invoke(File f, VirtualChannel channel) throws IOException, InterruptedException {
+    public List<String> invoke(File f, VirtualChannel vchannel) throws IOException, InterruptedException {
         try {
-            LOG.info("Executting `" + command.trim() + "` in" + f.toString() + " (" + f.getAbsolutePath() + ")");
-            Process process = new ProcessBuilder(command.trim().split(" "))
-                    .directory(f)
-                    .start();
-            OutputReader stdoutReader = new OutputReader(command, process.getInputStream()).start();
-            OutputReader stderrReader = new OutputReader(command, process.getErrorStream()).start();
-            if (!process.waitFor(60, TimeUnit.SECONDS)) {
-                throw new OperationFailedException("Command time out");
+            String stdout = null;
+            File kandidateFile = new File(f, command.trim());
+            if (kandidateFile.exists()) {
+                LOG.info("Reading " + command.trim() + " (" + kandidateFile.getAbsolutePath() + ")");
+                stdout = FileToString(kandidateFile);
+            } else {
+                LOG.info("Executing `" + command.trim() + "` in" + f.toString() + " (" + f.getAbsolutePath() + ")");
+                Process process = new ProcessBuilder(command.trim().split(" "))
+                        .directory(f)
+                        .start();
+                OutputReader stdoutReader = new OutputReader(command, process.getInputStream()).start();
+                OutputReader stderrReader = new OutputReader(command, process.getErrorStream()).start();
+                if (!process.waitFor(60, TimeUnit.SECONDS)) {
+                    throw new OperationFailedException("Command time out");
+                }
+                LOG.info("Returned " + process.exitValue());
+                Thread.sleep(10); //giving time to pipes to finish
+                try {
+                    process.getInputStream().close();
+                } catch (Exception ex) {
+                }
+                try {
+                    process.getErrorStream().close();
+                } catch (Exception ex) {
+                }
+                Thread.sleep(10); //giving time to pipes to finish
+                LOG.info("sout read " + stdoutReader.getBytes());
+                LOG.info("serr read " + stderrReader.getBytes());
+                if (stderrReader.getResult() != null) {
+                    throw new OperationFailedException(stderrReader.getResult());
+                }
+                stdout = stdoutReader.getResult();
             }
-            LOG.info("Returned " + process.exitValue());
-            Thread.sleep(10); //giving time to pipes to finish
-            try {
-                process.getInputStream().close();
-            } catch (Exception ex) {
-            }
-            try {
-                process.getErrorStream().close();
-            } catch (Exception ex) {
-            }
-            Thread.sleep(10); //giving time to pipes to finish
-            LOG.info("sout read " + stdoutReader.getBytes());
-            LOG.info("serr read " + stderrReader.getBytes());
-            if (stderrReader.getResult() != null) {
-                throw new OperationFailedException(stderrReader.getResult());
-            }
-            String stdout = stdoutReader.getResult();
             if (stdout == null) {
                 throw new OperationFailedException("Command produced no output");
             }
@@ -90,6 +101,26 @@ public class CommandCallable implements FileCallable<List<String>> {
         } catch (Exception ex) {
             throw new OperationFailedException(ex.toString());
         }
+    }
+
+    private String FileToString(File filePath) throws IOException {
+        try (FileInputStream fis = new FileInputStream(filePath)) {
+            return readStream(fis);
+        }
+    }
+
+    public static String readStream(InputStream is) throws IOException {
+        StringBuilder sb = new StringBuilder(512);
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"))) {
+            while (true) {
+                String s = br.readLine();
+                if (s == null) {
+                    break;
+                }
+                sb.append(s).append("\n");
+            }
+        }
+        return sb.toString();
     }
 
     @Override
