b1b8812430d04b6954efa3a47214d64434fe6847
Jiri Vanek <jvanek@redhat.com>
2023-05-12 12:44:42 +0200

    Reworked  to be on get instead of post

--- src/main/java/io/jenkins/plugins/report/genericdiff/DiffReportAction.java
+++ src/main/java/io/jenkins/plugins/report/genericdiff/DiffReportAction.java
@@ -23,21 +23,26 @@
  */
 package io.jenkins.plugins.report.genericdiff;
 
-import org.kohsuke.stapler.StaplerProxy;
 
+import org.kohsuke.stapler.StaplerRequest;
+import org.kohsuke.stapler.StaplerResponse;
+import org.kohsuke.stapler.WebMethod;
+import org.kohsuke.stapler.verb.GET;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.RandomAccessFile;
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
 
 import hudson.model.AbstractBuild;
 import hudson.model.Action;
-import hudson.model.Descriptor;
-import hudson.model.Job;
-import hudson.tasks.Publisher;
-import hudson.util.DescribableList;
 import jenkins.tasks.SimpleBuildStep;
 
-
-public class DiffReportAction implements Action, StaplerProxy, SimpleBuildStep.LastBuildAction {
+public class DiffReportAction implements SimpleBuildStep.LastBuildAction {
 
     private final AbstractBuild<?, ?> build;
 
@@ -45,14 +50,94 @@ public class DiffReportAction implements Action, StaplerProxy, SimpleBuildStep.L
         this.build = build;
     }
 
-    private RpmsReportPublisher getPublisher() {
-        DescribableList<Publisher, Descriptor<Publisher>> l = build.getProject().getPublishersList();
-        for (Publisher p : l.toArray(new Publisher[0])) {
-            if (p instanceof RpmsReportPublisher) {
-                return (RpmsReportPublisher) p;
+    private RpmsReport getRpmsReport(AbstractBuild<?, ?> build) {
+        final RpmsReport data;
+        Optional<RpmsReportPublisher> publisher = (build.getProject().getPublishersList().stream().filter(p -> p instanceof RpmsReportPublisher).map(a -> (RpmsReportPublisher) a).findFirst());
+        if (publisher.isPresent()) {
+            data = new RpmsReport(publisher.get(), build);
+        } else {
+            data = null;
+        }
+        return data;
+    }
+
+    public String getDisplayName() {
+        return DefaultStrings.DIFF_TITLE_REPORT;
+    }
+
+    @GET
+    @WebMethod(name = "dynamic-diff")
+    public void doSearchOffset(StaplerRequest req, StaplerResponse res) throws IOException {
+        res.setContentType("text/plain");
+        res.setCharacterEncoding("UTF-8");
+        PrintWriter out = res.getWriter();
+
+        try {
+            long from = Long.parseLong(req.getParameter("from"));
+            long to = Long.parseLong(req.getParameter("to"));
+        } catch (NumberFormatException e) {
+            res.setStatus(404);
+            out.print("Couldn't parse input");
+            out.flush();
+            return;
+        }
+
+        long from = Long.parseLong(req.getParameter("from"));
+        long to = Long.parseLong(req.getParameter("to"));
+
+        if (from <= 0 || to <= 0) {
+            res.setStatus(404);
+            out.print("Invalid parameters");
+        } else {
+            AbstractBuild<?, ?> b1 = this.build.getProject().getBuild(from + "");
+            AbstractBuild<?, ?> b2 = this.build.getProject().getBuild(to + "");
+            RpmsReport data1 = getRpmsReport(b1);
+            RpmsReport data2 = getRpmsReport(b2);
+            if (data1 == null && data2 == null) {
+                res.setStatus(404);
+                out.print("builds " + from + " nor " + to + " have valid data");
+            } else if (data1 == null) {
+                res.setStatus(404);
+                out.print("build " + from + " do not have have valid data");
+            } else if (data2 == null) {
+                res.setStatus(404);
+                out.print("build " + to + " do not have have valid data");
+            } else {
+                List<String> resultLines = comapre(data1, data2);
+                resultLines.add("jupiii!");
+                for (String resultLine : resultLines) {
+                    out.println(resultLine);
+                }
             }
         }
-        return null;
+        out.flush();
+    }
+
+    private List<String> comapre(RpmsReport data1, RpmsReport data2) {
+        List<String> resultLines = new ArrayList<>();
+        List<RpmsReportSingle> files1 = data1.getIndex();
+        for (RpmsReportSingle report : files1) {
+            if (report.getAllRpms()!=null) {
+                resultLines.addAll(report.getAllRpms());
+            }
+//            Patch<String> diff = DiffUtils.diff(l0, l1);
+//            List<String> unifiedDiff = UnifiedDiffUtils.generateUnifiedDiff(name0, name1, l0, diff, Math.max(lo.size, l1.size));
+        }
+        List<RpmsReportSingle> files2 = data2.getIndex();
+        for (RpmsReportSingle report : files2) {
+            if (report.getAllRpms()!=null) {
+                resultLines.addAll(report.getAllRpms());
+            }
+//            Patch<String> diff = DiffUtils.diff(l0, l1);
+//            List<String> unifiedDiff = UnifiedDiffUtils.generateUnifiedDiff(name0, name1, l0, diff, Math.max(lo.size, l1.size));
+        }
+        return resultLines;
+    }
+
+    @Override
+    public Collection<? extends Action> getProjectActions() {
+        ArrayList<Action> list = new ArrayList<>();
+        return list;
     }
 
     @Override
@@ -61,27 +146,17 @@ public class DiffReportAction implements Action, StaplerProxy, SimpleBuildStep.L
     }
 
     @Override
-    public String getDisplayName() {
-        return DefaultStrings.DIFF_TITLE_REPORT;
-    }
-
-    @Override
     public String getUrlName() {
         return DefaultStrings.PATCH_URL;
     }
 
-
-    @Override
-    public DiffReport getTarget() {
-        return new DiffReport(getPublisher(), build);
+    public String getThisBuild() {
+        return build.getId();
     }
 
-
-
-    @Override
-    public Collection<? extends Action> getProjectActions() {
-        Job<?, ?> job = build.getParent();
-        return Collections.singleton(new DiffReportProjectAction(job));
+    public String getPreviousBuild() {
+        return build.getPreviousCompletedBuild().getId();
     }
+
 
 }
